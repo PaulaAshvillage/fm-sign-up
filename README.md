efce785a2a744842299b09dcda527ed223867aee
Paula Aschendorf <paula.aschendorf@gmail.com>
2023-06-21 13:12:45 +0200

    adds README.md with to dos and learnings, adds input error messages and icon

---
+++ README.md
@@ -0,0 +1,17 @@
+#H1 Maike and Paulas Project
+
+## to do
+
+- placeholder no valid substitute for label: for accessbility write info text in label tag and hide content [hide content](https://www.a11yproject.com/posts/how-to-hide-content/)
+
+- uppercase styling via css (transform:uppercase)
+
+##learnings:
+
+- input validation check via css
+- prettier ignore possibilies
+- input and img elements can not have pseudo elements!
+- how to insert and position a svg in a input field
+- js: blur event (naming: blur is the opposite of focus in visualization)
+- working with for of loop to add a class name
+- js: add a class Name through js
--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const firstName = document.querySelector("#first-name");
 const lastName = document.querySelector("#last-name");
 const eMail = document.querySelector("#email");
 const password = document.querySelector("#password");
-
+const inputFields = document.querySelectorAll("input");
 const submitBtn = document.querySelector("#btn-trial");
+
+inputFields.forEach((inputField) => {
+  inputField.addEventListener("blur", function () {
+    inputField.classList.add("touched");
+  });
+});
 
 submitBtn.addEventListener("click", InputCheck);
 
 function InputCheck() {
   if (firstName.value === "");
   {
-    firstName.className = "input-error";
   }
 
   if (lastName.value === "");
@@ -28,6 +33,7 @@ function InputCheck() {
     console.log("falsch");
   }
 }
+
 //Farbe input
 //label/svg Lösung
 //regex email
